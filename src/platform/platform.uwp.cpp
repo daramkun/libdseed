58f25dc337113041e47254fa64789c302bfda848
Jin Jae-yeon <daramkun@live.com>
2019-08-21 17:08:52 +0900

    Fixed VS2019 Intellisense error about Platform.winmd, Windows.winmd

--- src/platform/platform.uwp.cpp
+++ src/platform/platform.uwp.cpp
@@ -51,13 +51,82 @@ namespace dseed
 class __uwp_application : public dseed::application
 {
 public:
+	__uwp_application ()
+		: _refCount (1)
+	{
 
+	}
+
+public:
+	virtual int32_t retain () override { return ++_refCount; }
+	virtual int32_t release () override
+	{
+		auto ret = --_refCount;
+		if (ret == 0)
+		{
+			delete this;
+			g_sharedApp = nullptr;
+		}
+		return ret;
+	}
+
+public:
+	virtual dseed::error_t input_device (dseed::inputdevice_t type, dseed::inputdevice** device) override
+	{
+
+	}
+
+public:
+	virtual dseed::error_t set_title (const char* title) override
+	{
+
+	}
+
+public:
+	virtual dseed::error_t client_size (dseed::size2i* size) override
+	{
+
+	}
+	virtual dseed::error_t set_client_size (const dseed::size2i& size) override
+	{
+
+	}
+
+public:
+	virtual dseed::error_t window_mode (dseed::windowmode_t* mode) override
+	{
+
+	}
+	virtual dseed::error_t set_window_mode (dseed::windowmode_t mode) override
+	{
+
+	}
+
+public:
+	virtual dseed::error_t event_handler (dseed::event_handler** handler) override
+	{
+
+	}
+	virtual dseed::error_t set_event_handler (dseed::event_handler* handler) override
+	{
+
+	}
+
+public:
+	virtual dseed::error_t run (dseed::event_handler* handler) override
+	{
+
+	}
+
+private:
+	std::atomic<int32_t> _refCount;
 };
 
 dseed::error_t dseed::create_application (dseed::application** app)
 {
 	if (g_sharedApp != nullptr) return dseed::error_invalid_op;
 	if (app == nullptr) return dseed::error_invalid_args;
+
 
 }
 
